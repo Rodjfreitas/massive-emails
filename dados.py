0fb4e00f8c6c4d0533fd4d560a98b6458d039f21
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-04-09 18:03:16 -0300

    feat: inclusã de funcionalidades de relatórios, correções de erros causados por diversos testes realizados

--- dados.py
+++ dados.py
@@ -1,4 +1,4 @@
-def enviarEmail(usuario, senha_usuario, assunto, destinatario, conteudo, caminho_anexo):
+def enviarEmail(usuario, senha_usuario, assunto, destinatario, com_copia, conteudo, caminho_anexo, assinatura, nome_destinatario):
     import os
     import smtplib
     # from email.message import EmailMessage
@@ -14,6 +14,9 @@ def enviarEmail(usuario, senha_usuario, assunto, destinatario, conteudo, caminho
     EMAIL_ADRESS = usuario
     EMAIL_PASSWORD = senha_usuario
 
+    # Criando uma lista para imprimir um aquivo de retorno
+    envio = {}
+
     try:
         # Startar o servidor
         host = 'smtp.gmail.com'
@@ -28,12 +31,16 @@ def enviarEmail(usuario, senha_usuario, assunto, destinatario, conteudo, caminho
         msg['Subject'] = assunto
         msg['From'] = usuario
         msg['To'] = destinatario
-        msg.attach(MIMEText(conteudo, 'plain'))
+        msg['cc'] = com_copia
+        msg.attach(
+            MIMEText(f'{conteudo}\n\n\nAtenciosamente, \n{assinatura}', 'plain'))
+        # msg.attach(MIMEText(f'\n\n\nAtenciosamente, \n{assinatura}', 'plain'))
 
         # Anexar arquivos
         pdf_folder = caminho_anexo
         pdf_files = [os.path.join(pdf_folder, f) for f in os.listdir(
             pdf_folder) if f.endswith('.pdf')]
+        qtd_files = 0
 
         for file in pdf_files:
             part = MIMEBase('application', 'octet-stream')
@@ -42,15 +49,38 @@ def enviarEmail(usuario, senha_usuario, assunto, destinatario, conteudo, caminho
             part.add_header('content-Disposition', 'attachment',
                             filename=os.path.basename(file))
             msg.attach(part)
+            qtd_files += 1
 
         # Enviar email
         server.sendmail(msg['From'], msg['To'], msg.as_string())
         server.quit()
-        print(f'\033[32mEmail Enviado\033[m - {assunto}')
+
+        # Informações para relatório de envio
+        envio['nome'] = nome_destinatario
+        envio['assunto'] = assunto
+        envio['email'] = destinatario
+        envio['status'] = 'Enviado'
+        if qtd_files == 0:
+            envio['obs'] = 'Não foi enviado arquivo(s). Vefificar Diretório Informado'
+            print(f'\033[33mAtençaõ: Enviado sem anexos.\033[m - {assunto}')
+        else:
+            if qtd_files == 1:
+                envio['obs'] = f'{qtd_files} arquivo enviado'
+            else:
+                envio['obs'] = f'{qtd_files} arquivos enviados'
+            print(f'\033[32mEmail Enviado\033[m - {assunto}')
+        return envio
     except:
         print(f'\033[31mEmail Não Enviado\033[m - {assunto}')
-        return 'Erro no envio'
-    return 'Sucesso no envio'
+        envio['nome'] = nome_destinatario
+        envio['assunto'] = assunto
+        envio['email'] = 'Inválido / Não informado'
+        envio['status'] = 'Não Enviado'
+        if qtd_files != 0:
+            envio['obs'] = 'Email Inválido ou não informado'
+        else:
+            envio['obs'] = 'Email Inválido ou não informado. Verificar também Diretório Informado.'
+        return envio
 
 
 def linha():
@@ -64,7 +94,7 @@ def titulo(msg):
 
 
 def menu(lista):
-    titulo('Massive Emails')
+    titulo('Mass&Mails')
     for pos, valor in enumerate(lista):
         print(f'{pos + 1:>3} -- {valor:30}')
     linha()
@@ -118,15 +148,47 @@ def buscarnoArquivo(arquivo):
     return valores
 
 
+def salvanoArquivo(lista):
+    import os
+    import openpyxl
+
+    nome_do_arquivo = 'Relatorio_emails_disparados.xlsx'
+    arquivo = openpyxl.Workbook()
+    planilha = arquivo.active
+    planilha.title = 'Relatório de emails disparados'
+    cabecalho_colunas = ['Nome', 'Assunto', 'Email', 'Obs:']
+
+    for coluna, cabecalho in enumerate(cabecalho_colunas, start=1):
+        planilha.cell(row=1, column=coluna).value = cabecalho
+
+    for linha, dados_linha in enumerate(lista, start=2):
+        for coluna, dado in enumerate(dados_linha.values(), start=1):
+            planilha.cell(row=linha, column=coluna).value = dado
+
+    # verifica se já existe um arquivo com mesmo nome.
+    if os.path.isfile(nome_do_arquivo):
+        # se o arquivo existe, exclui-o
+        os.remove(nome_do_arquivo)
+    arquivo.save(nome_do_arquivo)
+    print('\033[32mArquivo Salvo.\033[m')
+
+
 def imprimirLista(lista):
     titulo('RELAÇÃO PARA ENVIO')
     for pos, valor in enumerate(lista):
-        if pos == 0:
+        for campo in valor:
+            if campo is None:
+                campo == 'não informado'
+        try:
+            if pos == 0:
+                print(
+                    f'{valor[0]:23}{valor[1]:20}{valor[3]:50}{valor[4]:50}')
+                continue
             print(
-                f'{valor[0]:23}{valor[1]:20}{valor[3]:50}{valor[4]:50}')
+                f'{pos:3}{valor[0]:20}{valor[1]:20}{valor[3]:50}{valor[4]:50}')
+        except:
+            print(f'\033[31mAlgum dado de {valor[0]} está incompleto.\033[m')
             continue
-        print(
-            f'{pos:3}{valor[0]:20}{valor[1]:20}{valor[3]:50}{valor[4]:50}')
     linha()
 
 
@@ -143,3 +205,14 @@ def inserirdiretorio(nome):
         return False
     print('\033[32mArquivo carregado com sucesso\033[m')
     return True
+
+
+def questionSimNao(msg):
+    while True:
+        pergunta = input(f'{msg} [S/N]: ').upper().strip()[0]
+        if pergunta not in 'SN':
+            print('\033[31mInválido\033[m')
+            continue
+        if pergunta == 'S':
+            return 'S'
+        return 'N'
